376c484b4ed7ed6f26b7901d26d2c1dda40dea49
ye <juliet0727@126.com>
2022-06-07 14:13:21 +0100

    add a new window

---
+++ VisualThreading/ToolWindows/BuildingWindowControl.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace VisualThreading
+{
+    public partial class BuildingWindowControl : UserControl
+    {
+        public BuildingWindowControl()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            VS.MessageBox.Show("BuildingWindowControl", "Button clicked");
+        }
+    }
+}
