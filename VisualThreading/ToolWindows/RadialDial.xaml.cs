46c1e13c433388e0cae5fb2cca10ed7f1b79fd69
Jianxuan Cao <rogerscao0623@gmail.com>
2022-06-13 15:32:55 +0100

    Update RadialDial.xaml.cs
    Fill in menu items

--- VisualThreading/ToolWindows/RadialDial.xaml.cs
+++ VisualThreading/ToolWindows/RadialDial.xaml.cs
@@ -2,6 +2,7 @@ using RadialMenu.Controls;
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace VisualThreading.ToolWindows
 {
@@ -15,10 +16,14 @@ namespace VisualThreading.ToolWindows
 
             var mainMenuItems = new List<RadialMenuItem>
             {
-                new() { Content = new TextBlock { Text = "Thread" }, Background = Brushes.AliceBlue, ArrowBackground = Brushes.Firebrick}, //  "Background" take Brushes.color
-                new() { Content = new TextBlock { Text = "Test" }, Background = Brushes.AliceBlue, ArrowBackground = Brushes.Firebrick},
-                new() { Content = new TextBlock { Text = "Code" }, Background = Brushes.AliceBlue, ArrowBackground = Brushes.Firebrick},
-                new() { Content = new TextBlock { Text = "UI" }, Background = Brushes.AliceBlue, ArrowBackground = Brushes.Firebrick}
+                // I the know the color looks bad
+                // Background controls the Fan-shaped area
+                // ArrowBackground controls the littel arrow on each button
+                // EdgeBackground controls the Edge of the Fan-shaped 
+                new() { Content = new TextBlock { Text = "Thread" }, Background = Brushes.LightBlue, ArrowBackground = Brushes.LightBlue, EdgeBackground = Brushes.LightGreen}, 
+                new() { Content = new TextBlock { Text = "Test" }},
+                new() { Content = new TextBlock { Text = "Code" }},
+                new() { Content = new TextBlock { Text = "UI" }}
             };
 
             // Set default menu to Main menu
@@ -41,6 +46,38 @@ namespace VisualThreading.ToolWindows
                 new(){ Content = new TextBlock { Text = "Variable" } },
                 new(){ Content = new TextBlock { Text = "Operator" } },
                 new(){ Content = new TextBlock { Text = "Comparator" } }
+            };
+
+            var methodKeywordSubMenu = new List<RadialMenuItem>
+            {
+                new(){ Content = new TextBlock { Text = "Static" } },
+                new(){ Content = new TextBlock { Text = "Public" } },
+                new(){ Content = new TextBlock { Text = "Private" } },
+                new(){ Content = new TextBlock { Text = "Protected" } },
+                new(){ Content = new TextBlock { Text = "Internal" } },
+                new(){ Content = new TextBlock { Text = "Return" } }
+            };
+
+            var loopSubMenu = new List<RadialMenuItem>
+            {
+                new(){ Content = new TextBlock { Text = "Break" } },
+                new(){ Content = new TextBlock { Text = "Continue" } },
+                new(){ Content = new TextBlock { Text = "For" } },
+                new(){ Content = new TextBlock { Text = "While" } }
+            }; 
+
+            var operatorSubMenu = new List<RadialMenuItem>
+            {
+                new(){ Content = new TextBlock { Text = "equal" } },
+                new(){ Content = new TextBlock { Text = "plus" } },
+                new(){ Content = new TextBlock { Text = "minus" } },
+                new(){ Content = new TextBlock { Text = "multiply" } },
+                new(){ Content = new TextBlock { Text = "divide" } },
+                new(){ Content = new TextBlock { Text = "modulus" } },
+                new(){ Content = new TextBlock { Text = "and" } },
+                new(){ Content = new TextBlock { Text = "or" } },
+                new(){ Content = new TextBlock { Text = "()" } }
+
             };
 
             var uiSubMenu = new List<RadialMenuItem>
@@ -71,6 +108,13 @@ namespace VisualThreading.ToolWindows
             // Back to Home on center item
             MainMenu.CentralItem.Click += (sender, e) =>
                 RadialDialControl_Click(sender, e, "MainMenuItems");
+
+
+            mainMenuItems[0].MouseEnter += (sender, e) =>
+                RadialDialControl_Hover(sender, e, "ThreadSubMenu");  // handles the mouse hover action, display corresponding preview 
+
+            mainMenuItems[0].MouseLeave += (sender, e) =>  // handles the mouse left hover action(when mouse leaves a button), clear the preview area
+                RadialDialControl_ExitHover(sender, e);
         }
 
         private void RadialDialControl_Click(object sender, RoutedEventArgs e, String subMenu)
@@ -78,7 +122,28 @@ namespace VisualThreading.ToolWindows
             ThreadHelper.JoinableTaskFactory.RunAsync(async () =>
             {
                 await Task.Delay(100);
+                Console.WriteLine("sssss");
                 MainMenu.Items = _menuCollection[subMenu];
+            }
+            ).FireAndForget();
+        }
+
+        private void RadialDialControl_Hover(object sender, RoutedEventArgs e, String PreviewName)
+        {
+            ThreadHelper.JoinableTaskFactory.RunAsync(async () =>
+            {
+                await Task.Delay(100);
+                // take the name of the element and pull png file from json.
+            }
+            ).FireAndForget();
+        }
+
+        private void RadialDialControl_ExitHover(object sender, RoutedEventArgs e)
+        {
+            ThreadHelper.JoinableTaskFactory.RunAsync(async () =>
+            {
+                await Task.Delay(100);
+                // clear preview area
             }
             ).FireAndForget();
         }
