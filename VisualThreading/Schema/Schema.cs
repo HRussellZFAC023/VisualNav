04b649b6fc5a1a2951e1120497ac970ea79e9643
Jianxuan Cao <jianxuancao@outlook.com>
2022-06-18 18:56:04 +0100

    change json format

--- VisualThreading/Schema/Schema.cs
+++ VisualThreading/Schema/Schema.cs
@@ -1,20 +1,17 @@
-﻿using Newtonsoft.Json;
+﻿using System.Threading.Tasks;
+using Newtonsoft.Json;
 using System.IO;
-using System.Threading.Tasks;
 
 namespace VisualThreading.Schema
 {
     public class Schema
     {
-        private Schema()
-        { /*(static members only)*/ }
-
         public Radialmenu[] RadialMenu { get; set; }
 
         public static async Task<Schema> LoadAsync()
         {
             var dir = Path.GetDirectoryName(typeof(Schema).Assembly.Location);
-            var file = Path.Combine(dir!, "Schema", "schema.json");
+            var file = Path.Combine(dir!, "Schema", "Schema.json");
 
             using var reader = new StreamReader(file);
             var json = await reader.ReadToEndAsync();
@@ -22,19 +19,99 @@ namespace VisualThreading.Schema
         }
     }
 
-    public class Radialmenu
+    public class Rootobject
     {
-        public string FileExt { get; set; }
-        public string Text { get; set; }
-        public Command[] Commands { get; set; }
-        public object[] Ui { get; set; }
-        public object[] Symbols { get; set; }
+        public Radialmenu[] RadialMenu { get; set; }
     }
 
-    public class Command
+    public class Radialmenu
     {
-        public string Text { get; set; }
-        public string Color { get; set; }
-        public string Preview { get; set; }
+        public string fileExt { get; set; }
+        public string text { get; set; }
+        public Thread[] Thread { get; set; }
+        public Test[] Test { get; set; }
+        public Code[] Code { get; set; }
+        public UI[] UI { get; set; }
+    }
+
+    public class Thread
+    {
+        public string text { get; set; }
+        public string preview { get; set; }
+        public string color { get; set; }
+    }
+
+    public class Test
+    {
+        public string text { get; set; }
+        public string preview { get; set; }
+        public string color { get; set; }
+    }
+
+    public class Code
+    {
+        public IO[] IO { get; set; }
+        public MethodKeyword[] MethodKeyword { get; set; }
+        public Condition[] Condition { get; set; }
+        public Loop[] Loop { get; set; }
+        public Variable[] Variables { get; set; }
+        public Operator[] Operator { get; set; }
+        public Comparetor[] Comparetor { get; set; }
+    }
+
+    public class IO
+    {
+        public string text { get; set; }
+        public string preview { get; set; }
+        public string color { get; set; }
+    }
+
+    public class MethodKeyword
+    {
+        public string text { get; set; }
+        public string preview { get; set; }
+        public string color { get; set; }
+    }
+
+    public class Condition
+    {
+        public string text { get; set; }
+        public string preview { get; set; }
+        public string color { get; set; }
+    }
+
+    public class Loop
+    {
+        public string text { get; set; }
+        public string preview { get; set; }
+        public string color { get; set; }
+    }
+
+    public class Variable
+    {
+        public string text { get; set; }
+        public string preview { get; set; }
+        public string color { get; set; }
+    }
+
+    public class Operator
+    {
+        public string text { get; set; }
+        public string preview { get; set; }
+        public string color { get; set; }
+    }
+
+    public class Comparetor
+    {
+        public string text { get; set; }
+        public string preview { get; set; }
+        public string color { get; set; }
+    }
+
+    public class UI
+    {
+        public string text { get; set; }
+        public string preview { get; set; }
+        public string color { get; set; }
     }
 }
