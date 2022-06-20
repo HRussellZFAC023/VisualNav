3cfcb4c68853ddf1f6d2f09b743435c9f48073b8
Jianxuan Cao <jianxuancao@outlook.com>
2022-06-20 15:13:17 +0100

    auto generate menu structure, TODO: implement auto generation of click handling

--- VisualThreading/Schema/Schema.cs
+++ VisualThreading/Schema/Schema.cs
@@ -7,7 +7,6 @@ namespace VisualThreading.Schema
     public class Schema
     {
         public Radialmenu[] RadialMenu { get; set; }
-
         public static async Task<Schema> LoadAsync()
         {
             var dir = Path.GetDirectoryName(typeof(Schema).Assembly.Location);
@@ -19,99 +18,27 @@ namespace VisualThreading.Schema
         }
     }
 
-    public class Rootobject
-    {
-        public Radialmenu[] RadialMenu { get; set; }
-    }
-
     public class Radialmenu
     {
         public string fileExt { get; set; }
         public string text { get; set; }
-        public Thread[] Thread { get; set; }
-        public Test[] Test { get; set; }
-        public Code[] Code { get; set; }
-        public UI[] UI { get; set; }
+        public Menuitem[] MenuItems { get; set; }
+        public Command[] commands { get; set; }
     }
 
-    public class Thread
+    public class Menuitem
+    {
+        public string name { get; set; }
+        public string parent { get; set; }
+    }
+
+    public class Command
     {
         public string text { get; set; }
+        public string parent { get; set; }
         public string preview { get; set; }
         public string color { get; set; }
     }
 
-    public class Test
-    {
-        public string text { get; set; }
-        public string preview { get; set; }
-        public string color { get; set; }
-    }
 
-    public class Code
-    {
-        public IO[] IO { get; set; }
-        public MethodKeyword[] MethodKeyword { get; set; }
-        public Condition[] Condition { get; set; }
-        public Loop[] Loop { get; set; }
-        public Variable[] Variables { get; set; }
-        public Operator[] Operator { get; set; }
-        public Comparetor[] Comparetor { get; set; }
-    }
-
-    public class IO
-    {
-        public string text { get; set; }
-        public string preview { get; set; }
-        public string color { get; set; }
-    }
-
-    public class MethodKeyword
-    {
-        public string text { get; set; }
-        public string preview { get; set; }
-        public string color { get; set; }
-    }
-
-    public class Condition
-    {
-        public string text { get; set; }
-        public string preview { get; set; }
-        public string color { get; set; }
-    }
-
-    public class Loop
-    {
-        public string text { get; set; }
-        public string preview { get; set; }
-        public string color { get; set; }
-    }
-
-    public class Variable
-    {
-        public string text { get; set; }
-        public string preview { get; set; }
-        public string color { get; set; }
-    }
-
-    public class Operator
-    {
-        public string text { get; set; }
-        public string preview { get; set; }
-        public string color { get; set; }
-    }
-
-    public class Comparetor
-    {
-        public string text { get; set; }
-        public string preview { get; set; }
-        public string color { get; set; }
-    }
-
-    public class UI
-    {
-        public string text { get; set; }
-        public string preview { get; set; }
-        public string color { get; set; }
-    }
 }
--- VisualThreading/ToolWindows/RadialWindowControl.xaml.cs
+++ VisualThreading/ToolWindows/RadialWindowControl.xaml.cs
@@ -1,7 +1,9 @@
 using RadialMenu.Controls;
 using System.Collections;
 using System.Collections.Generic;
+using System.Windows;
 using System.Windows.Controls;
+
 
 namespace VisualThreading.ToolWindows
 {
@@ -14,183 +16,48 @@ namespace VisualThreading.ToolWindows
         public RadialWindowControl()
         {
             InitializeComponent();
+            RadialMenuGeneration();
 
-            // comment out code use for icon displaying
-            // StackPanel threadPanel = new StackPanel();
-            // threadPanel.Children.Add(new Image { Source = new BitmapImage(new Uri("D:/visualthreading/VisualThreading/Resources/Icon.png", UriKind.RelativeOrAbsolute)) });
-            // threadPanel.Children.Add(new TextBlock { Text = "Thread" });
 
-            var mainMenuItems = new List<RadialMenuItem>
-            {
-                // I the know the color looks bad
-                // Background controls the Fan-shaped area
-                // ArrowBackground controls the littel arrow on each button
-                // EdgeBackground controls the Edge of the Fan-shaped
-                // new() { Content = threadPanel},
-                new() { Content = new TextBlock { Text = "Thread" }},
-                new() { Content = new TextBlock { Text = "Test" }},
-                new() { Content = new TextBlock { Text = "Code" }},
-                new() { Content = new TextBlock { Text = "UI" }}
-            };
-            _menuCollection.Add("MainMenuItems", mainMenuItems);
-            mainMenuItems[0].Click += (_, _) => RadialDialControl_Click("ThreadSubMenu"); // Go to ThreadSubMenu
-            mainMenuItems[1].Click += (_, _) => RadialDialControl_Click("TestSubMenu"); // Go to TestSubMenu
-            mainMenuItems[2].Click += (_, _) => RadialDialControl_Click("CodeSubMenu"); // Go to CodeSubMenu
-            mainMenuItems[3].Click += (_, _) => RadialDialControl_Click("UISubMenu"); // Go to UISubMenu
-
-            // Set default menu to Main menu
-            MainMenu.Items = mainMenuItems;
-
-            // Layer 1 sub menu
-            var threadSubMenu = new List<RadialMenuItem> {
-                new() { Content = new TextBlock { Text = "New Thread " } }
-            };
-            var testSubMenu = new List<RadialMenuItem> {
-                new() { Content = new TextBlock { Text = "Assert" } }
-            };
-            var codeSubMenu = new List<RadialMenuItem>
-            {
-                new(){ Content = new TextBlock { Text = "I/O" } },
-                new(){ Content = new TextBlock { Text = "Method/Keyword" } },
-                new(){ Content = new TextBlock { Text = "Condition" } },
-                new(){ Content = new TextBlock { Text = "Loop" } },
-                new(){ Content = new TextBlock { Text = "Variable" } },
-                new(){ Content = new TextBlock { Text = "Operator" } },
-                new(){ Content = new TextBlock { Text = "Comparator" } }
-            };
-            var uiSubMenu = new List<RadialMenuItem>
-            {
-                new() { Content = new TextBlock { Text = "Sub Item 1" } },
-                new() { Content = new TextBlock { Text = "Sub Item 2" } },
-                new() { Content = new TextBlock { Text = "Sub Item 3" } }
-            };
-            _menuCollection.Add("ThreadSubMenu", threadSubMenu);
-            _menuCollection.Add("TestSubMenu", testSubMenu);
-            _menuCollection.Add("CodeSubMenu", codeSubMenu);
-            _menuCollection.Add("UISubMenu", uiSubMenu);
-
-            // -----------------------------ThreadSubMenu-------------------------------------------------------------------
-            threadSubMenu[0].Click += (_, _) => RadialDialElement_Click("New Thread");
-            threadSubMenu[0].MouseEnter += (_, _) => RadialDialElement_Hover("New Thread");  // handles the mouse hover action, display corresponding preview
-            threadSubMenu[0].MouseLeave += (_, _) => RadialDialElement_ExitHover(); // handles the mouse left hover action(when mouse leaves a button), clear the preview area
-            // -------------------------------------------------------------------------------------------------------------
-
-            // -----------------------------Test sub menu-------------------------------------------------------------------
-            testSubMenu[0].Click += (_, _) => RadialDialElement_Click("Assert");
-            testSubMenu[0].MouseEnter += (_, _) => RadialDialElement_Hover("Assert");  // handles the mouse hover action, display corresponding preview
-            testSubMenu[0].MouseLeave += (_, _) => RadialDialElement_ExitHover(); // handles the mouse left hover action(when mouse leaves a button), clear the preview area
-            // -------------------------------------------------------------------------------------------------------------
-
-            var ioSubMenu = new List<RadialMenuItem>
-            {
-                new(){ Content = new TextBlock { Text = "Print" } },
-                new(){ Content = new TextBlock { Text = "Write" } },
-                new(){ Content = new TextBlock { Text = "ReadKey" } },
-                new(){ Content = new TextBlock { Text = "UserInput" } },
-                new(){ Content = new TextBlock { Text = "UserInputLine" } },
-                new(){ Content = new TextBlock { Text = "OpenText" } },
-                new(){ Content = new TextBlock { Text = "WriteToFile" } }
-            };
-            var methodKeywordSubMenu = new List<RadialMenuItem>
-            {
-                new(){ Content = new TextBlock { Text = "Static" } },
-                new(){ Content = new TextBlock { Text = "Public" } },
-                new(){ Content = new TextBlock { Text = "Private" } },
-                new(){ Content = new TextBlock { Text = "Protected" } },
-                new(){ Content = new TextBlock { Text = "Internal" } },
-                new(){ Content = new TextBlock { Text = "Return" } }
-            };
-            var conditionKeywordSubMenu = new List<RadialMenuItem>
-            {
-                new(){ Content = new TextBlock { Text = "If" } },
-                new(){ Content = new TextBlock { Text = "Else" } },
-                new(){ Content = new TextBlock { Text = "ElseIf" } },
-                new(){ Content = new TextBlock { Text = "Switch" } },
-                new(){ Content = new TextBlock { Text = "Case" } },
-            };
-            conditionKeywordSubMenu[0].Click += (_, _) => RadialDialElement_Click("If");
-            conditionKeywordSubMenu[0].MouseEnter += (_, _) => RadialDialElement_Hover("If");  // handles the mouse hover action, display corresponding preview
-            conditionKeywordSubMenu[0].MouseLeave += (_, _) => RadialDialElement_ExitHover(); // handles the mouse left hover action(when mouse leaves a button), clear the preview area
-            conditionKeywordSubMenu[1].Click += (_, _) => RadialDialElement_Click("Else");
-            conditionKeywordSubMenu[1].MouseEnter += (_, _) => RadialDialElement_Hover("Else");  // handles the mouse hover action, display corresponding preview
-            conditionKeywordSubMenu[1].MouseLeave += (_, _) => RadialDialElement_ExitHover(); // handles the mouse left hover action(when mouse leaves a button), clear the preview area
-            conditionKeywordSubMenu[2].Click += (_, _) => RadialDialElement_Click("ElseIf");
-            conditionKeywordSubMenu[2].MouseEnter += (_, _) => RadialDialElement_Hover("ElseIf");  // handles the mouse hover action, display corresponding preview
-            conditionKeywordSubMenu[2].MouseLeave += (_, _) => RadialDialElement_ExitHover(); // handles the mouse left hover action(when mouse leaves a button), clear the preview area
-            conditionKeywordSubMenu[3].Click += (_, _) => RadialDialElement_Click("Switch");
-            conditionKeywordSubMenu[3].MouseEnter += (_, _) => RadialDialElement_Hover("Switch");  // handles the mouse hover action, display corresponding preview
-            conditionKeywordSubMenu[3].MouseLeave += (_, _) => RadialDialElement_ExitHover(); // handles the mouse left hover action(when mouse leaves a button), clear the preview area
-            conditionKeywordSubMenu[4].Click += (_, _) => RadialDialElement_Click("Case");
-            conditionKeywordSubMenu[4].MouseEnter += (_, _) => RadialDialElement_Hover("Case");  // handles the mouse hover action, display corresponding preview
-            conditionKeywordSubMenu[4].MouseLeave += (_, _) => RadialDialElement_ExitHover(); // handles the mouse left hover action(when mouse leaves a button), clear the preview area
-                                                                                              // -------------------------------------------------------------------------------------------------------------
-
-            var loopSubMenu = new List<RadialMenuItem>
-            {
-                new(){ Content = new TextBlock { Text = "Break" } },
-                new(){ Content = new TextBlock { Text = "Continue" } },
-                new(){ Content = new TextBlock { Text = "For" } },
-                new(){ Content = new TextBlock { Text = "While" } }
-            };
-            var variableSubMenu = new List<RadialMenuItem>
-            {
-                new(){ Content = new TextBlock { Text = "Void" } },
-                new(){ Content = new TextBlock { Text = "Integer" } },
-                new(){ Content = new TextBlock { Text = "Double" } },
-                new(){ Content = new TextBlock { Text = "Character" }},
-                new(){ Content = new TextBlock { Text = "Boolean" } },
-                new(){ Content = new TextBlock { Text = "String" } },
-                new(){ Content = new TextBlock { Text = "Array" } },
-            };
-            var operatorSubMenu = new List<RadialMenuItem>
-            {
-                new(){ Content = new TextBlock { Text = "Equal" } },
-                new(){ Content = new TextBlock { Text = "Plus" } },
-                new(){ Content = new TextBlock { Text = "Minus" } },
-                new(){ Content = new TextBlock { Text = "Multiply" } },
-                new(){ Content = new TextBlock { Text = "Divide" } },
-                new(){ Content = new TextBlock { Text = "Modulus" } },
-                new(){ Content = new TextBlock { Text = "And" } },
-                new(){ Content = new TextBlock { Text = "Or" } },
-                new(){ Content = new TextBlock { Text = "()" } }
-            };
-            var comparatorMenu = new List<RadialMenuItem>
-            {
-                new(){ Content = new TextBlock { Text = "&&" } },
-                new(){ Content = new TextBlock { Text = "||" } },
-                new(){ Content = new TextBlock { Text = "==" } },
-                new(){ Content = new TextBlock { Text = ">" } },
-                new(){ Content = new TextBlock { Text = "<" } },
-                new(){ Content = new TextBlock { Text = ">=" } },
-                new(){ Content = new TextBlock { Text = "<=" } },
-                new(){ Content = new TextBlock { Text = "!=" } },
-                new(){ Content = new TextBlock { Text = "()" } },
-            };
-            _menuCollection.Add("IOSubMenu", ioSubMenu);
-            _menuCollection.Add("methodKeywordSubMenu", methodKeywordSubMenu);
-            _menuCollection.Add("conditionKeywordSubMenu", conditionKeywordSubMenu);
-            _menuCollection.Add("loopSubMenu", loopSubMenu);
-            _menuCollection.Add("variableSubMenu", variableSubMenu);
-            _menuCollection.Add("operatorSubMenu", operatorSubMenu);
-            _menuCollection.Add("comparatorMenu", comparatorMenu);
-
-            codeSubMenu[0].Click += (_, _) => RadialDialControl_Click("IOSubMenu"); // Go to Input/Ouput
-            codeSubMenu[1].Click += (_, _) => RadialDialControl_Click("methodKeywordSubMenu"); // Go to Keyword/Decorator
-            codeSubMenu[2].Click += (_, _) => RadialDialControl_Click("conditionKeywordSubMenu");// Go to Keyword/Decorator
-            codeSubMenu[3].Click += (_, _) => RadialDialControl_Click("loopSubMenu");// Go to Loop
-            codeSubMenu[4].Click += (_, _) => RadialDialControl_Click("variableSubMenu");// Go to Variables
-            codeSubMenu[5].Click += (_, _) => RadialDialControl_Click("operatorSubMenu");// Go to Operator
-            codeSubMenu[6].Click += (_, _) => RadialDialControl_Click("comparatorMenu");// Go to Comparator
-            // -------------------------------------------------------------------------------------------------------------
-
-            // -----------------------------UI sub menu---------------------------------------------------------------------
-            uiSubMenu[0].Click += (_, _) => RadialDialElement_Click("UISubMenu");
-            uiSubMenu[0].MouseEnter += (_, _) => RadialDialElement_Hover("UISubMenu");  // handles the mouse hover action, display corresponding preview
-            uiSubMenu[0].MouseLeave += (_, _) => RadialDialElement_ExitHover(); // handles the mouse left hover action(when mouse leaves a button), clear the preview area
-            // -------------------------------------------------------------------------------------------------------------
 
             // Back to Home on center item
             MainMenu.CentralItem.Click += (_, _) => RadialDialControl_Back();
+        }
+
+        private void RadialMenuGeneration()
+        {
+            ThreadHelper.JoinableTaskFactory.RunAsync(async () =>
+            {
+                var json = await Schema.Schema.LoadAsync();
+                var buffer = await VS.Documents.GetActiveDocumentViewAsync();
+                IDictionary<string, List<RadialMenuItem>> menu = new Dictionary<string, List<RadialMenuItem>>(); // Store all menu levels without heriachy
+
+                foreach (var language in json.RadialMenu)
+                {
+                    foreach (var menuItem in language.MenuItems)
+                    {
+                        if (menu.ContainsKey(menuItem.parent))
+                        {
+                            menu[menuItem.parent].Add(new() { Content = new TextBlock { Text = menuItem.name } });
+                        }
+                        else
+                        {
+                            menu.Add(menuItem.parent, new List<RadialMenuItem> { });
+                            menu[menuItem.parent].Add(new() { Content = new TextBlock { Text = menuItem.name } });
+                        }
+                    }  // Generate the menu structure to the menu dictionary 
+
+                    MainMenu.Items = menu["Main"];  // Set default menu to Main menu
+
+                    foreach (var command in language.commands)
+                    {
+                        menu[command.parent].Add(new() { Content = new TextBlock { Text = command.text } });
+                    }  // Generate the command structure to the menu dictionary 
+
+                }  // For each Language, generate menu, commands, links, and handler
+
+            }
+            ).FireAndForget();
         }
 
         private void RadialDialElement_Click(string element) // handles the eventuall element like a veriable
