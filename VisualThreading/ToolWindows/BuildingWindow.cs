feddbd08e7d2d65f2eb71a9694edaabe3f550b85
Henry Russell <henry@therussellhouse.com>
2022-06-23 16:51:51 +0100

    fix building window ui and read files async

--- VisualThreading/ToolWindows/BuildingWindow.cs
+++ VisualThreading/ToolWindows/BuildingWindow.cs
@@ -1,10 +1,10 @@
-﻿using System.IO;
-using Microsoft.VisualStudio.Imaging;
+﻿using Microsoft.VisualStudio.Imaging;
+using Microsoft.VisualStudio.Text;
+using System.IO;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using Microsoft.VisualStudio.Text;
 using VisualThreading.ToolWindows;
 
 namespace VisualThreading
@@ -22,13 +22,28 @@ namespace VisualThreading
             var commands = await Schema.Schema.LoadAsync();
             var buffer = await VS.Documents.GetActiveDocumentViewAsync();
             var fileExt = "";
+
+            // note reading from files should be done async or we will have lots of issues
+            var root = Path.GetDirectoryName(typeof(VisualStudioServices).Assembly.Location);
+            var blockly = await ReadFileAsync(Path.Combine(root!, "Resources", "html", "blocklyHTML.html"));
+            var toolbox = await ReadFileAsync(Path.Combine(root!, "Resources", "xml", "blocklyToolbox.xml"));
+            var workspace = await ReadFileAsync(Path.Combine(root!, "Resources", "xml", "blocklyWorkspace.xml"));
+
             if (buffer?.TextBuffer != null)
             {
                 fileExt =
                     Path.GetExtension(buffer.TextBuffer.GetFileName());
             }
-            Instance = new BuildingWindowControl(commands, fileExt);
+
+            Instance = new BuildingWindowControl(commands, fileExt, blockly, toolbox, workspace);
             return Instance;
+        }
+
+        private static async Task<string> ReadFileAsync(string file)
+        {
+            using var reader = new StreamReader(file);
+            var content = await reader.ReadToEndAsync();
+            return content;
         }
 
         [Guid("6a0155f8-b16a-4fba-90bb-8c9fab68de1b")]
--- VisualThreading/ToolWindows/BuildingWindowControl.xaml.cs
+++ VisualThreading/ToolWindows/BuildingWindowControl.xaml.cs
@@ -6,12 +6,19 @@ namespace VisualThreading.ToolWindows
 {
     public partial class BuildingWindowControl : UserControl
     {
-        public BuildingWindowControl(Schema.Schema commands, string fileExt)
+        private Schema.Command currentCommand;
+        private readonly string _workspace;
+        private readonly string _toolbox;
+
+        public BuildingWindowControl(Schema.Schema commands, string fileExt, string blockly, string toolbox, string workspace)
         {
             InitializeComponent();
             ShowCodeButton.IsEnabled = false;
+            currentCommand = null;
+            _toolbox = toolbox;
+            _workspace = workspace;
 
-            Browser.NavigateToString(System.IO.File.ReadAllText("../../Resources/html/blocklyHTML.html"));
+            Browser.NavigateToString(blockly);
         }
 
         private void ShowCodeButton_Click(object sender, RoutedEventArgs e)
@@ -23,15 +30,22 @@ namespace VisualThreading.ToolWindows
         private void WebBrowser_LoadCompleted(object sender, NavigationEventArgs e)
         {
             ShowCodeButton.IsEnabled = true;
-            var toolboxXML = System.IO.File.ReadAllText("../../Resources/xml/blocklyToolbox.xml");
-            var workspaceXML = System.IO.File.ReadAllText("../../Resources/xml/blocklyWorkspace.xml");
-            //Initialize blocky using toolbox and workspace
-            Browser.InvokeScript("init", new object[] { toolboxXML, workspaceXML });
+            Browser.InvokeScript("init", _toolbox, _workspace);  //Initialize blocky using toolbox and workspace
         }
 
         public void SetCurrentCommand(Schema.Command c)
         {
-            // TODO
+            // Color: #FF00FFFF
+            // Parent: Loop
+            // Preview: for ( statement 1; statement 2; statement 3 ){\n  statements;\n}
+            // Text: for
+            currentCommand = c;
+            var color = c.Color;
+            var parent = c.Parent;
+            var preview = c.Preview;
+            var text = c.Text;
+
+            Browser.InvokeScript("addNewBlockToArea", parent, text, color);
         }
     }
 }
