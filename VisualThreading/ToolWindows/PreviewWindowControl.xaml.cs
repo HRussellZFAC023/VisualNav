fb3cbd7bdab75ad40a57f100787ec93e9411c56d
Henry Russell <henry@therussellhouse.com>
2022-07-01 19:00:47 +0100

    Update PreviewWindowControl.xaml.cs

--- VisualThreading/ToolWindows/PreviewWindowControl.xaml.cs
+++ VisualThreading/ToolWindows/PreviewWindowControl.xaml.cs
@@ -36,7 +36,7 @@ namespace VisualThreading.ToolWindows
 
             ThreadHelper.JoinableTaskFactory.RunAsync(async () =>
             {
-                await Browser.EvaluateScriptAsync("init", _toolbox, _workspace, false);
+                await Browser.EvaluateScriptAsync("init", _toolbox, _workspace, true);
             }).FireAndForget();
         }
 
