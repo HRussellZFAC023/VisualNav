63ad1d95063ed3a51d69cbd49f9c3a3ffa56b530
Qinglan Ye <juliet0727@126.com>
2022-07-04 21:42:39 +0800

    insert to code editor / copy to clipboard

--- VisualThreading/ToolWindows/BuildingWindowControl.xaml.cs
+++ VisualThreading/ToolWindows/BuildingWindowControl.xaml.cs
@@ -9,7 +9,7 @@ namespace VisualThreading.ToolWindows
     public partial class BuildingWindowControl
     {
         private Command currentCommand;
-        private string language;
+        private string _currentLanguage;
         private readonly string _workspace;
         private readonly string _toolbox;
         private readonly dynamic _schema;
@@ -20,11 +20,13 @@ namespace VisualThreading.ToolWindows
             _toolbox = toolbox;
             _workspace = workspace;
             _schema = JsonConvert.DeserializeObject(schema);
+            _currentLanguage = fileExt;
 
             InitializeComponent();
             Focus();
             Browser.LoadHtml(blockly);
 
+            VS.Events.SelectionEvents.SelectionChanged += SelectionEventsOnSelectionChanged;
             Browser.LoadingStateChanged += BrowserOnLoadingStateChanged;
         }
 
@@ -42,28 +44,124 @@ namespace VisualThreading.ToolWindows
 
                 Browser.ExecuteScriptAsync(await fr.ReadFileAsync(Path.Combine(blockly, "blockly_compressed.js")));
                 Browser.ExecuteScriptAsync(await fr.ReadFileAsync(Path.Combine(blockly, "blocks_compressed.js")));
-                Browser.ExecuteScriptAsync(await fr.ReadFileAsync(Path.Combine(blockly, "csharp_compressed.js")));
-
                 Browser.ExecuteScriptAsync(await fr.ReadFileAsync(Path.Combine(blockly, "msg", "js", "en.js")));
+
+                // import code generator
+                Browser.ExecuteScriptAsync(await fr.ReadFileAsync(Path.Combine(blockly, "csharp_compressed.js")));
+                Browser.ExecuteScriptAsync(await fr.ReadFileAsync(Path.Combine(blockly, "javascript_compressed.js")));
+                Browser.ExecuteScriptAsync(await fr.ReadFileAsync(Path.Combine(blockly, "python_compressed.js")));
+                Browser.ExecuteScriptAsync(await fr.ReadFileAsync(Path.Combine(blockly, "php_compressed.js")));
+                Browser.ExecuteScriptAsync(await fr.ReadFileAsync(Path.Combine(blockly, "dart_compressed.js")));
+                Browser.ExecuteScriptAsync(await fr.ReadFileAsync(Path.Combine(blockly, "lua_compressed.js")));
 
 
                 await Browser.EvaluateScriptAsync("init", _toolbox, _workspace, false);
+
             }).FireAndForget();
+        }
+
+        private void SelectionEventsOnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var fileExt = "";
+            if (e.To != null)
+            {
+                var buffer = e.To.Name;
+                fileExt =
+                        Path.GetExtension(buffer);
+            }
+            _currentLanguage = fileExt;
         }
 
         private void ShowCodeButton_Click(object sender, RoutedEventArgs e)
         {
             ThreadHelper.JoinableTaskFactory.RunAsync(async () =>
             {
-                var result = await Browser.EvaluateScriptAsync(
-                    "showCode", new object[] { });
-
-                if (result.Success != true)
+                if(_currentLanguage != null)
                 {
-                    System.Windows.MessageBox.Show(result.Message);
+                    var result = await Browser.EvaluateScriptAsync(
+                    "showCode", _currentLanguage);
 
+                    if (result.Success != true)
+                    {
+                        System.Windows.MessageBox.Show(result.Message);
+
+                    }
+                    else
+                    {
+                        System.Windows.MessageBox.Show((string)result.Result);
+
+                    }
+                } else
+                {
+                    System.Windows.MessageBox.Show("please select a file(.js .cs .py .php)");
                 }
-                System.Windows.MessageBox.Show((string)result.Result);
+
+
+            }).FireAndForget();
+        }
+
+        private void InsertCodeButton_Click(object sender, RoutedEventArgs e)
+        {
+            ThreadHelper.JoinableTaskFactory.RunAsync(async () =>
+            {
+
+                if (_currentLanguage != null)
+                {
+                    var result = await Browser.EvaluateScriptAsync(
+                    "showCode", _currentLanguage);
+
+                    if (result.Success != true)
+                    {
+                        System.Windows.MessageBox.Show(result.Message);
+
+                    }
+                    else
+                    {
+                        var re = "";
+
+                        re = (string)result.Result;
+                        DocumentView docView = await VS.Documents.GetActiveDocumentViewAsync();
+                        if (docView?.TextView == null) return;
+                        Microsoft.VisualStudio.Text.SnapshotPoint position = docView.TextView.Caret.Position.BufferPosition;
+                        docView.TextBuffer?.Insert(position, re);
+                    }
+                }
+                else
+                {
+                    System.Windows.MessageBox.Show("please select a file(.js .cs .py .php)");
+                }
+
+
+            }).FireAndForget();
+        }
+
+        private void ClipboardButton_Click(object sender, RoutedEventArgs e)
+        {
+            ThreadHelper.JoinableTaskFactory.RunAsync(async () =>
+            {
+                if (_currentLanguage != null)
+                {
+                    var result = await Browser.EvaluateScriptAsync(
+                    "showCode", _currentLanguage);
+
+                    if (result.Success != true)
+                    {
+                        System.Windows.MessageBox.Show(result.Message);
+
+                    }
+                    else
+                    {
+                        var re = "";
+                        re = (string)result.Result;
+                        Clipboard.SetText(re);
+
+                        System.Windows.MessageBox.Show("Copy Successfully!");
+                    }
+                }
+                else
+                {
+                    System.Windows.MessageBox.Show("please select a file(.js .cs .py .php)");
+                }
             }).FireAndForget();
         }
 
