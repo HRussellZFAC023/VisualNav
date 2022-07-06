86b9d77940b2595ed2c81eb517764dad15ffda5b
Henry Russell <henry@therussellhouse.com>
2022-07-07 00:23:34 +0100

    fix bug

--- VisualThreading/ToolWindows/PreviewWindow.cs
+++ VisualThreading/ToolWindows/PreviewWindow.cs
@@ -19,21 +19,7 @@ namespace VisualThreading.ToolWindows
 
         public override async Task<FrameworkElement> CreateAsync(int toolWindowId, CancellationToken cancellationToken)
         {
-            var fr = new FileReaderAdapter();
-            var schema = await Schema.Schema.LoadAsync();
-
-            var buffer = await VS.Documents.GetActiveDocumentViewAsync();
-            var root = Path.GetDirectoryName(typeof(VisualStudioServices).Assembly.Location);
-            var blockly = await fr.ReadFileAsync(Path.Combine(root!, "Resources", "html", "blocklyHTML.html"));
-
-            var fileExt = "";
-            if (buffer?.TextBuffer != null)
-            {
-                fileExt =
-                Path.GetExtension(buffer.TextBuffer.GetFileName());
-            }
-
-            Instance = new PreviewWindowControl(schema, fileExt);
+            Instance = new PreviewWindowControl();
             return Instance;
         }
 
