510e62d0bddc1de595121a8a84a02c3dff28482d
Henry Russell <henry@therussellhouse.com>
2022-07-22 17:59:28 +0100

    fix master

--- VisualNav/Commands/OpenAllVisualNavWindows.cs
+++ VisualNav/Commands/OpenAllVisualNavWindows.cs
@@ -1,12 +1,11 @@
-﻿namespace VisualNav.Commands
+﻿namespace VisualNav.Commands;
+
+//[Command("<insert guid from .vsct file>", 0x0100)]
+//[Command(PackageIds.OpenBuildingWindow)]
+internal sealed class OpenAllVisualNavWindows : BaseCommand<OpenAllVisualNavWindows>
 {
-    //[Command("<insert guid from .vsct file>", 0x0100)]
-    [Command(PackageIds.OpenBuildingWindow)]
-    internal sealed class OpenAllVisualNavWindows : BaseCommand<OpenAllVisualNavWindows>
+    protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
     {
-        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
-        {
-            await VS.MessageBox.ShowWarningAsync("OpenAllVisualNavWindows", "Button clicked");
-        }
+        await VS.MessageBox.ShowWarningAsync("OpenAllVisualNavWindows", "Button clicked");
     }
 }
