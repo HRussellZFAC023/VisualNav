b55544b0eb3ca58b14d4d20109dcb7f8e0110079
Henry Russell <henry@therussellhouse.com>
2022-08-12 09:57:50 +0100

    bug fix

--- VisualNav/ToolWindows/PreviewWindowControl.xaml.cs
+++ VisualNav/ToolWindows/PreviewWindowControl.xaml.cs
@@ -49,7 +49,7 @@ public partial class PreviewWindowControl
             Text = c.Description,
             TextWrapping = TextWrapping.Wrap,
             Width = RootGrid.Width,
-            FontSize = c.Description.Length > 70 ? GetFontSize() : GetFontSize() / 2
+            FontSize = c.Description.Length < 70 ? GetFontSize() : GetFontSize() / 1.5
         });
         }).FireAndForget();
     }
@@ -75,7 +75,7 @@ public partial class PreviewWindowControl
                     Text = m.Description,
                     TextWrapping = TextWrapping.Wrap,
                     Width = RootGrid.Width,
-                    FontSize = m.Description.Length > 70 ? GetFontSize() : GetFontSize() / 2
+                    FontSize = m.Description.Length < 70 ? GetFontSize() : GetFontSize() / 1.5
                 });
         }).FireAndForget();
     }
